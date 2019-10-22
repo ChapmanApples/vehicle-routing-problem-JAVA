93152d6411e66fd31136075c5fc9a1103b126f55
ChapmanApples <102115618@student.swin.edu.au>
2019-10-22 13:28:32 +1100

    commit

--- VehicleRoutingProblem/src/RoutingAgents/Node.java
+++ VehicleRoutingProblem/src/RoutingAgents/Node.java
@@ -3,14 +3,14 @@ package RoutingAgents;
 public class Node {
 	public String name;
 	public int ID;
-	public int parcels;
+	public int parcels_weight;
 	public int x_pos;
 	public int y_pos;
 	
 	public Node(String Name, int id, int Parcels, int X_pos, int Y_pos) {
 		name = Name;
 		ID = id;
-		parcels = Parcels;
+		parcels_weight = Parcels;
 		x_pos = X_pos;
 		y_pos = Y_pos;
 	}
--- VehicleRoutingProblem/src/RoutingAgents/Truck.java
+++ VehicleRoutingProblem/src/RoutingAgents/Truck.java
@@ -1,10 +1,12 @@
 package RoutingAgents;
 
+import java.util.ArrayList;
+
 public class Truck {
 	public String TruckID;
 	public int holding_capacity = 0;
 	public int weight_capacity = 10;
-	public Node[] locations;
+	public ArrayList<Node> locations = new ArrayList<Node>();
 	
 	public Truck(String ID) {
 		TruckID = ID;
